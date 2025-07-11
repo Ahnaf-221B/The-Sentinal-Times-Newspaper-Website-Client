69b774f68c8c09cd512dca4b6747ab8c4a4751c2
Ahnaf-221B <mostafaahnafhossain@gmail.com>
2025-07-11 22:08:06 +0600

    publisher list added to ui

--- src/pages/Publisher/PublisherCard.jsx
+++ src/pages/Publisher/PublisherCard.jsx
@@ -1,20 +1,20 @@
-// import React from "react";
+import React from "react";
 
-// const PublisherCard = ({ logoUrl, publisherName }) => {
-// 	return (
-// 		<div className="bg-stone-200 shadow-lg p-4 rounded-lg hover:shadow-xl transition duration-300 ease-in-out">
-// 			<div className="text-center">
-// 				<img
-// 					src={logoUrl}
-// 					alt={publisherName}
-// 					className="w-24 h-24 object-cover mx-auto rounded-full"
-// 				/>
-// 				<h3 className="text-xl font-semibold text-gray-800 mt-4">
-// 					{publisherName}
-// 				</h3>
-// 			</div>
-// 		</div>
-// 	);
-// };
+const PublisherCard = ({ logoUrl, publisherName }) => {
+	return (
+		<div className="bg-stone-200 shadow-lg p-4 rounded-lg hover:shadow-xl transition duration-300 ease-in-out">
+			<div className="text-center">
+				<img
+					src={logoUrl}
+					alt={publisherName}
+					className="w-24 h-24 object-cover mx-auto rounded-full"
+				/>
+				<h3 className="text-xl font-semibold text-gray-800 mt-4">
+					{publisherName}
+				</h3>
+			</div>
+		</div>
+	);
+};
 
-// export default PublisherCard;
+export default PublisherCard;
--- src/pages/Publisher/PublisherList.jsx
+++ src/pages/Publisher/PublisherList.jsx
@@ -1,41 +1,41 @@
-// import React, { useEffect, useState } from "react";
-// import useAxios from "../../hooks/useAxios"; // Assuming you have a custom Axios hook
-// import PublisherCard from "./PublisherCard"; // Import PublisherCard Component
+import React, { useEffect, useState } from "react";
+import useAxios from "../../hooks/useAxios"; // Assuming you have a custom Axios hook
+import PublisherCard from "./PublisherCard"; // Import PublisherCard Component
 
-// const PublisherList = () => {
-// 	const [publishers, setPublishers] = useState([]);
-// 	const axiosInstance = useAxios();
+const PublisherList = () => {
+	const [publishers, setPublishers] = useState([]);
+	const axiosInstance = useAxios();
 
-// 	useEffect(() => {
-// 		// Fetch publishers from the API
-// 		const fetchPublishers = async () => {
-// 			try {
-// 				const response = await axiosInstance.get("/publishers");
-// 				setPublishers(response.data); // Set publishers data from API response
-// 			} catch (error) {
-// 				console.error("Error fetching publishers", error);
-// 			}
-// 		};
+	useEffect(() => {
+		// Fetch publishers from the API
+		const fetchPublishers = async () => {
+			try {
+				const response = await axiosInstance.get("/publishers");
+				setPublishers(response.data); // Set publishers data from API response
+			} catch (error) {
+				console.error("Error fetching publishers", error);
+			}
+		};
 
-// 		fetchPublishers();
-// 	}, [axiosInstance]);
+		fetchPublishers();
+	}, [axiosInstance]);
 
-// 	return (
-// 		<div className="container mx-auto p-6">
-// 			<h2 className="text-3xl font-semibold text-center mb-8 mt-12">Publishers</h2>
-// 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-12">
-// 				{/* Render Publisher Card for each publisher */}
-// 				{publishers.map((publisher) => (
-// 					<div key={publisher._id}>
-// 						<PublisherCard
-// 							logoUrl={publisher.logoUrl}
-// 							publisherName={publisher.publisherName}
-// 						/>
-// 					</div>
-// 				))}
-// 			</div>
-// 		</div>
-// 	);
-// };
+	return (
+		<div className="container mx-auto p-6">
+			<h2 className="text-3xl font-semibold text-center mb-8 mt-12">Publishers</h2>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-12">
+				{/* Render Publisher Card for each publisher */}
+				{publishers.map((publisher) => (
+					<div key={publisher._id}>
+						<PublisherCard
+							logoUrl={publisher.logoUrl}
+							publisherName={publisher.publisherName}
+						/>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
 
-// export default PublisherList;
+export default PublisherList;
